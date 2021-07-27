f3e4cb08a7b9ceb5a03bf21f2c770247809cf8a8
sagarmhaske7088 <86242988+sagarmhaske7088@users.noreply.github.com>
2021-07-27 18:10:06 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int a,b,c;
+    cout <<"Enter the number "<<endl;
+    cin>>a>>b;
+    c=a+b;
+    cout<<"addition is ="<<c<<endl;
+    c=a-b;
+    cout<<"substraction is ="<<c<<endl;
+    c=a*b;
+    cout<<"multipliction is ="<<c<<endl;
+    c=a%b;
+    cout<<"Division is ="<<c<<endl;
+    return 0;
+}
+
+
+//output :
+
+12
+23
+
+
+Output:
+
+Enter the number 
+addition is =35
+substraction is =-11
+multipliction is =276
+Division is =12
